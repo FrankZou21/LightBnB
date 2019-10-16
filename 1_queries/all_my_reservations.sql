de1047c3dd63b7331e7b29ba48dcdaaca61a4d17
Frank <frankzou2000@hotmail.com>
2019-10-16 17:18:10 +0000

    added 1-queries

---
+++ 1_queries/all_my_reservations.sql
@@ -0,0 +1,10 @@
+SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+-- SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating) AS average_rating
+FROM properties
+JOIN reservations ON properties.id = reservations.property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND now()::date >= reservations.end_date
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date
+LIMIT 10;
